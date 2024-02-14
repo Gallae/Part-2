c5023110a712caf57176f8e96129348206201e86
workmano <workmano@OA-S141-12>
2024-02-14 10:22:45 -0500

    task 1 complete

--- Assets/Week 5/Scripts/ReplacementKnight.cs
+++ Assets/Week 5/Scripts/ReplacementKnight.cs
@@ -3,7 +3,6 @@ using UnityEngine.EventSystems;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 public class ReplacementKnight : MonoBehaviour
 {
---
+++ Assets/Week 6/Scripts/SceneLoader.cs
@@ -0,0 +1,16 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = (currentSceneIndex + 1) % SceneManager.sceneCountInBuildSettings;
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
